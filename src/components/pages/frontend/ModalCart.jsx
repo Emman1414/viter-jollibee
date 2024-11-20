7e9509caffa775aeb6119c0709ed0fce4a707699
liempot <leemagno9@gmail.com>
2024-11-20 13:28:33 +0800

    how

--- src/components/pages/frontend/ModalCart.jsx
+++ src/components/pages/frontend/ModalCart.jsx
@@ -15,9 +15,10 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Processing from "./Processing";
 import Payment from "./Payment";
+import Dining from "./Dining";
 
 const ModalCart = ({ setShowCart, cartData, setCartData, getTotal }) => {
-  const [option, setOption] = React.useState(false);
+  const [option, setOption] = React.useState("cart");
 
    const navigate = useNavigate();
 
@@ -138,46 +139,7 @@ const ModalCart = ({ setShowCart, cartData, setCartData, getTotal }) => {
               </div>
             )}
           </div>
-        ) : option === "dining" ? (
-          <div className="max-w-[600px] w-full bg-white rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden">
-            <div className="modal-header p-4 flex justify-between items-center border-b border-gray-200">
-              <button onClick={() => setOption("cart")}>
-                <ArrowLeft />
-              </button>
-              <h5 className="mb-0">Chose One</h5>
-              <button>
-                <X />
-              </button>
-            </div>
-
-            <div className="madal-main h-[60vh] w-full flex justify-center items-center">
-              <div className="flex flex-col items-center">
-                <img
-                  src={`${imgPath}/jollibee-logo.png`}
-                  alt=""
-                  className="mx-auto mb-5 w-[90px]"
-                />
-
-                <p className="text-2xl mb-5 font-bold">
-                  Select preferred dining option
-                </p>
-                <button
-                  className="bg-primary px-8 py-4 w-[250px] flex justify-center gap-2 font-bold text-white items-center rounded-md"
-                  onClick={() => setOption("payment")}
-                >
-                  <Utensils /> Dine In
-                </button>
-                <h3 className="my-3">or</h3>
-                <button
-                  className="bg-primary px-8 py-4 w-[250px] flex justify-center gap-2 font-bold text-white items-center rounded-md"
-                  onClick={() => setOption("payment")}
-                >
-                  <ShoppingBag /> Take Out
-                </button>
-              </div>
-            </div>
-          </div>
-        ) : option === "payment" ? (<Payment/>) : (<Processing/>)}
+        ) : option === "dining" ? (<Dining/>) : option === "payment" ? (<Payment/>) : (<Processing/>)}
       </div>
     </>
   );
